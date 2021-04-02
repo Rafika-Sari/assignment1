fc3e4874f3d19884658598116e201d8da1f457bb
Rafika Sari <rafikasari2909@gmail.com>
2021-04-03 04:38:01 +0700

    file tambahan

---
+++ list.py
@@ -0,0 +1,13 @@
+mylist = []
+mylist.append("kalkulus 1")
+mylist.append("kalkulus 2")
+mylist.append("fisika dasar 1")
+mylist.append("fisika dasar 2")
+#print(mylist[0]) #prints 1
+#print(mylist[1]) #prints 2
+#print(mylist[2]) #prints 3
+#print(mylist[3]) #prints 4
+
+# printout 1,2,3,4
+for x in mylist:
+    print(x)
---
+++ string_operations.py
@@ -0,0 +1,10 @@
+#a = "Hello, World!"
+#print(a[1])
+#print(a[2:5])
+#print(len(a))
+
+b = "Hello"
+c = "World"
+#d = b + c
+d = b + " " + c
+print(d)
